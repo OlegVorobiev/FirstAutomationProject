fab6fb19364a0ed5918147d4d5a6f1ce6d40f407
Oleg <olegvorobiev15@gmail.com>
2020-08-16 23:14:00 +0700

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата =" + area(s));
+
+        Rectangle r = new Rectangle(1,5);
+        System.out.println("Площадь прямоугольника =" + area(r));
+
+    }
+    public static double area(Square s){
+        return s.l * s.l;
+    }
+    public static double area(Rectangle r){
+        return r.a * r.b;
     }
 }
